f82136fc46b8a24a31ca7f7ed6822ba916e96689
YaghoMattos <mattosyagho@gmail.com>
2021-06-16 23:46:43 -0300

    feature: initi parse and connect to Back4App

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Parse from "parse/react-native.js";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+Parse.setAsyncStorage(AsyncStorage);
+Parse.initialize('m0GlXlzavxfoYkdEfQOfcvg3P57xCrcQDhTwJ0O8','fKv7jVaceov8sgadLXeGa6HFpPS3UrUOrpCyAhjw');
+Parse.serverURL = 'https://parseapi.back4app.com/';
+
 export default function App() {
   return (
     <View style={styles.container}>
