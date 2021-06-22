fd015706c1440b9f66a5e44632e339e3e8122dec
YaghoMattos <mattosyagho@gmail.com>
2021-06-22 17:57:46 -0300

    feature: test create patient and organization

--- App.js
+++ App.js
@@ -1,68 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-import Parse from "parse/react-native.js";
+import Parse from 'parse/react-native.js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import { createPatient } from './src/Patient';
+
 Parse.setAsyncStorage(AsyncStorage);
-Parse.initialize('m0GlXlzavxfoYkdEfQOfcvg3P57xCrcQDhTwJ0O8','fKv7jVaceov8sgadLXeGa6HFpPS3UrUOrpCyAhjw');
+Parse.initialize(
+  'm0GlXlzavxfoYkdEfQOfcvg3P57xCrcQDhTwJ0O8',
+  'fKv7jVaceov8sgadLXeGa6HFpPS3UrUOrpCyAhjw'
+);
 Parse.serverURL = 'https://parseapi.back4app.com/';
 
 const App = () => {
-  const [person, setPerson] = useState(new Parse.Object('Person'));
+  const [patient, setPatient] = useState(new Parse.Object('Patient'));
 
-  async function addPerson() {
-    try {
-      //create a new Parse Object instance
-      const newPerson = new Parse.Object('Person');
-      //define the attributes you want for your Object
-      newPerson.set('name', 'John');
-      newPerson.set('email', 'john@back4app.com');
-      //save it on Back4App Data Store
-      await newPerson.save();
-    } catch (error) {
-      console.log('Error saving new person: ', error);
-    }
-  }
+  //Fetch Patiente
+  async function fetchPatient() {
+    let query = new Parse.Query('Patient');
 
-  async function fetchPerson() {
-    //create your Parse Query using the Person Class you've created
-    let query = new Parse.Query('Person');
-    // use the equalTo filter to look for user which the name is John. this filter can be used in any data type
-    query.equalTo('name', 'John');
-    //run the query
+    query.equalTo('name', 'Paciente Teste');
+
     let queryResult = await query.find();
-    //pick the first result 
-    const currentPerson = queryResult[0];
-    //access the Parse Object attributes
-    console.log('person id: ', currentPerson.get('id'));
-    console.log('person name: ', currentPerson.get('name'));
-    console.log('person email: ', currentPerson.get('email'));
-    setPerson(currentPerson);
-  }
 
-  useEffect(() => {
-    fetchPerson()
-  }, []);
+    const currentPatient = queryResult[0];
+
+    alert('Pacient returned !');
+    console.log('Paciente nome: ', currentPatient.get('name'));
+    console.log('Paciente email: ', currentPatient.get('email'));
+    setPatient(currentPatient);
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Name: {person.get('name')}</Text>
-      <Text>email: {person.get('email')}</Text>
-      <Button title='Add person' onPress={addPerson} />
-      <Button title='Fetch person' onPress={fetchPerson} />
+      <Text>Name: {patient.get('name')}</Text>
+      <Text>email: {patient.get('email')}</Text>
+      <Button title="Add patient" onPress={createPatient} />
+      <Button title="Fetch patient" onPress={fetchPatient} />
     </View>
-  )
-
-}
+  );
+};
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    alignItems:'center',
-    justifyContent:'space-evenly',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
   },
 });
