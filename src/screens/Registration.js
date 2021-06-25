d2bbdd9348afdecbfda281d0ec12c1d0cf53d65c
YaghoMattos <mattosyagho@gmail.com>
2021-06-25 03:25:40 -0300

    feature: admin registration screen

---
+++ src/screens/Registration.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { SafeAreaView, StatusBar, Text, View } from 'react-native';
+
+import { AdminRegistration } from '../components/AdminRegister';
+
+import Styles from '../components/Styles';
+
+export function Registration() {
+  return (
+    <>
+      <StatusBar />
+      <SafeAreaView style={Styles.login_container}>
+        <View style={Styles.login_header}>
+          <Text style={Styles.login_header_text}>
+            <Text style={Styles.login_header_text_bold}>
+              {'AppReabilitação - '}
+            </Text>
+            {' Admin registration'}
+          </Text>
+        </View>
+        <AdminRegistration />
+      </SafeAreaView>
+    </>
+  );
+}
