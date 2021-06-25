8d2321f509095bf66d3858c3a4ea581a36140642
YaghoMattos <mattosyagho@gmail.com>
2021-06-25 03:30:03 -0300

    style: style for screens

---
+++ src/components/Styles.js
@@ -0,0 +1,74 @@
+import { StyleSheet } from 'react-native';
+
+const Styles = StyleSheet.create({
+  login_container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  login_header: {
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingBottom: 20,
+    backgroundColor: '#208AEC',
+  },
+  login_header_text: {
+    marginTop: 15,
+    color: '#f0f0f0',
+    fontSize: 16,
+  },
+  login_header_text_bold: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  login_wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingVertical: 40,
+    borderTopRightRadius: 12,
+    borderTopLeftRadius: 12,
+    marginTop: -10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    maxWidth: 280,
+  },
+  form_input: {
+    height: 40,
+    paddingHorizontal: 20,
+    marginBottom: 20,
+    backgroundColor: '#EDF0F7',
+    borderRadius: 50,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 25,
+    backgroundColor: '#0065A4',
+    borderRadius: 50,
+  },
+  button_label: {
+    color: '#fff',
+    fontSize: 15,
+  },
+  login_footer_text: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    color: '#808080',
+    fontSize: 15,
+  },
+  login_footer_link: {
+    color: '#208AEC',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  home_container: {
+    marginTop: 100,
+    justifyContent: 'space-between',
+    width: '100%',
+    maxWidth: 600,
+  },
+});
+
+export default Styles;
