e42ebfd91dbd4d237a6f0a8e41c0d94f2b7340ff
YaghoMattos <mattosyagho@gmail.com>
2021-06-25 03:27:31 -0300

    feature: move to chat component

---
+++ src/components/ToChat.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import Styles from './Styles';
+
+export const ToChat = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={Styles.login_wrapper}>
+      <View style={Styles.form}>
+        <TouchableOpacity onPress={() => navigation.navigate('Chat')}>
+          <View style={Styles.button}>
+            <Text style={Styles.button_label}>{'Chat'}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
