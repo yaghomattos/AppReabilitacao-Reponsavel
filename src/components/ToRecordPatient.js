cb62d8f07ef0dc2b9d2e732e3261c3b6ec337912
YaghoMattos <mattosyagho@gmail.com>
2021-06-29 02:33:37 -0300

    feature: to record patient component

---
+++ src/components/ToRecordPatient.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import Styles from './Styles';
+
+export const ToRecordPatient = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={Styles.login_wrapper}>
+      <View style={Styles.form}>
+        <TouchableOpacity onPress={() => navigation.navigate('PatientRecord')}>
+          <View style={Styles.button}>
+            <Text style={Styles.button_label}>{'Novo Paciente'}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
