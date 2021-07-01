b06eb7952bacef2052b717b0f30ee8c686565f29
YaghoMattos <mattosyagho@gmail.com>
2021-07-01 12:31:41 -0300

    refactor: routes

--- src/routes/ToChat.js
+++ src/routes/ToChat.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-import Styles from './Styles';
+import Styles from '../components/Styles';
 
-export const ToRecordPatient = () => {
+export const ToChat = () => {
   const navigation = useNavigation();
   return (
     <View style={Styles.login_wrapper}>
       <View style={Styles.form}>
-        <TouchableOpacity onPress={() => navigation.navigate('PatientRecord')}>
+        <TouchableOpacity onPress={() => navigation.navigate('Chat')}>
           <View style={Styles.button}>
-            <Text style={Styles.button_label}>{'Novo Paciente'}</Text>
+            <Text style={Styles.button_label}>{'Chat'}</Text>
           </View>
         </TouchableOpacity>
       </View>
--- src/routes/ToRecordPatient.js
+++ src/routes/ToRecordPatient.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-import Styles from './Styles';
+import Styles from '../components/Styles';
 
-export const ToChat = () => {
+export const ToRecordPatient = () => {
   const navigation = useNavigation();
   return (
     <View style={Styles.login_wrapper}>
       <View style={Styles.form}>
-        <TouchableOpacity onPress={() => navigation.navigate('Chat')}>
+        <TouchableOpacity onPress={() => navigation.navigate('PatientRecord')}>
           <View style={Styles.button}>
-            <Text style={Styles.button_label}>{'Chat'}</Text>
+            <Text style={Styles.button_label}>{'Novo Paciente'}</Text>
           </View>
         </TouchableOpacity>
       </View>
