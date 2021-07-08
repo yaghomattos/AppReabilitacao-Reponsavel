ba4f9d96d7eb56e5fc4997b3777352f91d7f356e
YaghoMattos <mattosyagho@gmail.com>
2021-07-08 12:24:32 -0300

    feature: patient control screen

---
+++ src/screens/PatientControl.js
@@ -0,0 +1,32 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { SafeAreaView, StatusBar, Text, View } from 'react-native';
+
+import { Hello } from '../components/Hello';
+import { ToRecordPatient } from '../routes/ToRecordPatient';
+import { ToAppPatient } from '../routes/ToAppPatient';
+import { ToDeletePatient } from '../routes/ToDeletePatient';
+
+import Styles from '../components/Styles';
+
+export function PatientControl() {
+  return (
+    <>
+      <StatusBar />
+      <SafeAreaView style={Styles.login_container}>
+        <View style={Styles.login_header}>
+          <Text style={Styles.login_header_text}>
+            <Text style={Styles.login_header_text_bold}>
+              {'AppReabilitação - '}
+            </Text>
+            {'Controle Paciente'}
+          </Text>
+        </View>
+        <Hello />
+        <ToRecordPatient />
+        <ToDeletePatient />
+        <ToAppPatient />
+      </SafeAreaView>
+    </>
+  );
+}
