050540c3777adf5a5da6932707d248036777bce6
YaghoMattos <mattosyagho@gmail.com>
2021-07-14 21:37:56 -0300

    feature: list patients

---
+++ src/screens/ListPatients.js
@@ -0,0 +1,40 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+
+import { List, Divider } from 'react-native-paper';
+import { useParseQuery } from '@parse/react-native';
+
+const parseQuery = new Parse.Query('Patient');
+parseQuery.descending('name');
+
+export const ListPatients = () => {
+  const { isLive, isLoading, isSyncing, results, count, error, reload } =
+    useParseQuery(parseQuery);
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#f5f5f5',
+      flex: 1,
+    },
+    listTitle: {
+      fontSize: 22,
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={results}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={() => <Divider />}
+        renderItem={({ item }) => (
+          <List.Item
+            title={item.get('name')}
+            titleNumberOfLines={1}
+            titleStyle={styles.listTitle}
+          />
+        )}
+      />
+    </View>
+  );
+};
