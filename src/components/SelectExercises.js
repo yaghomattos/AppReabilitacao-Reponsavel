e5b2e2ecefc45c2da7fec92620b543dfc4a92b08
YaghoMattos <mattosyagho@gmail.com>
2021-07-22 16:53:48 -0300

    feature: crud select exercises

---
+++ src/components/SelectExercises.js
@@ -0,0 +1,76 @@
+import Parse from 'parse/react-native.js';
+
+export function createSelectExercises() {
+  const myNewObject = new Parse.Object('SelectExercises');
+  myNewObject.set('Patient', new Parse.Object('Patient'));
+  myNewObject.set('Exercise', new Parse.Object('Exercise'));
+  myNewObject.set('name', 'A string');
+  try {
+    const result = await myNewObject.save();
+    console.log('SelectExercises created', result);
+  } catch (error) {
+    console.error('Error while creating SelectExercises: ', error);
+  }
+}
+
+export function readSelectExercises() {
+  const SelectExercises = Parse.Object.extend('SelectExercises');
+  const query = new Parse.Query(SelectExercises);
+  // You can also query by using a parameter of an object
+  // query.equalTo('objectId', 'xKue915KBG');
+  const results = await query.find();
+  try {
+    const results = await query.find();
+    for (const object of results) {
+      // Access the Parse Object attributes using the .GET method
+      const Patient = object.get('Patient');
+      const Exercise = object.get('Exercise');
+      const name = object.get('name');
+      console.log(Patient);
+      console.log(Exercise);
+      console.log(name);
+    }
+  } catch (error) {
+    console.error('Error while fetching SelectExercises', error);
+  }
+}
+
+export function updateSelectExercises() {
+  const query = new Parse.Query(SelectExercises);
+  try {
+    // here you put the objectId that you want to update
+    const object = await query.get('xKue915KBG');
+    object.set('Patient', new Parse.Object("Patient"));
+    object.set('Exercise', new Parse.Object("Exercise"));
+    object.set('name', 'A string');
+    try {
+      const response = await object.save();
+      // You can use the "get" method to get the value of an attribute
+      // Ex: response.get("<ATTRIBUTE_NAME>")
+      // Access the Parse Object attributes using the .GET method
+      console.log(response.get('Patient'));
+      console.log(response.get('Exercise'));
+      console.log(response.get('name'));
+      console.log('SelectExercises updated', response);
+    } catch (error) {
+      console.error('Error while updating SelectExercises', error);
+      }
+    } catch (error) {
+      console.error('Error while retrieving object SelectExercises', error);
+    }
+}
+
+export function deleteSelectExercises() {
+  const query = new Parse.Query('SelectExercises');
+  try {
+    const object = await query.get('xKue915KBG');
+    try {
+      const response = await object.destroy();
+      console.log('Deleted ParseObject', response);
+    } catch (error) {
+      console.error('Error while deleting ParseObject', error);
+    }
+  } catch (error) {
+    console.error('Error while retrieving ParseObject', error);
+  }
+}
