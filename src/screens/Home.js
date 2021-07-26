abfed89a6f33d0f1d54cd7e647779517119010d6
YaghoMattos <mattosyagho@gmail.com>
2021-07-26 14:49:03 -0300

    feature: remove hello, upload exercise, useContext

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,36 +1,38 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useContext } from 'react';
 import { SafeAreaView, StatusBar, Text, View } from 'react-native';
 
 import { Logout } from '../components/Logout';
-import { Hello } from '../components/Hello';
 import { ToChat } from '../routes/ToChat';
 import { ToPatientControl } from '../routes/ToPatientControl';
-import { ToListPatient } from '../routes/ToListPatient';
-//import { ToUploadExercise } from '../routes/ToUploadExercise';
+import { ToListPatientSelectExercise } from '../routes/ToListPatientSelectExercise';
+import { ToUploadExercise } from '../routes/ToUploadExercise';
+
+import PatientContext from '../components/patientContext';
 
 import Styles from '../components/Styles';
 
 export function Home() {
   return (
     <>
-      <StatusBar />
-      <SafeAreaView style={Styles.login_container}>
-        <View style={Styles.login_header}>
-          <Text style={Styles.login_header_text}>
-            <Text style={Styles.login_header_text_bold}>
-              {'AppReabilitação - '}
+      <PatientContext.Provider value={{}}>
+        <StatusBar />
+        <SafeAreaView style={Styles.login_container}>
+          <View style={Styles.login_header}>
+            <Text style={Styles.login_header_text}>
+              <Text style={Styles.login_header_text_bold}>
+                {'AppReabilitação - '}
+              </Text>
+              {'Menu'}
             </Text>
-            {'Menu'}
-          </Text>
-        </View>
-        <Hello />
-        <ToChat />
-        <ToPatientControl />
-        {/* <ToUploadExercise /> */}
-        <ToListPatient />
-        <Logout />
-      </SafeAreaView>
+          </View>
+          <ToChat />
+          <ToPatientControl />
+          <ToListPatientSelectExercise />
+          <ToUploadExercise />
+          <Logout />
+        </SafeAreaView>
+      </PatientContext.Provider>
     </>
   );
 }
