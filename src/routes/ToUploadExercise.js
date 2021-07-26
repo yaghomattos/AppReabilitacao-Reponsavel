49ff9b7b9eb8ba701b7d63f111b9742e92fcbeae
YaghoMattos <mattosyagho@gmail.com>
2021-07-26 14:42:33 -0300

    feature: route to upload exercise

---
+++ src/routes/ToUploadExercise.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import Styles from '../components/Styles';
+
+export const ToUploadExercise = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={Styles.login_wrapper}>
+      <View style={Styles.form}>
+        <TouchableOpacity onPress={() => navigation.navigate('UploadExercise')}>
+          <View style={Styles.button}>
+            <Text style={Styles.button_label}>{'Adicionar Exercício'}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
