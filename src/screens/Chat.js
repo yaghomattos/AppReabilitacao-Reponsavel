6ebdbce7a546a52975a20c75f9c4fc8d3b3f66e2
YaghoMattos <mattosyagho@gmail.com>
2021-07-30 23:29:42 -0300

    fix: update chat for two peoples

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,38 +1,38 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
 
 import Parse from 'parse/react-native.js';
 import { useParseQuery } from '@parse/react-native';
 
-//Parse.enableLocalDatastore();
-
 const parseQuery = new Parse.Query('Chat');
 parseQuery.descending('createdAt');
 
-export function Chat() {
-  var currentUser = '';
+export function Chat(props) {
+  var currentUser = {
+    __type: 'Pointer',
+    className: '_User',
+    objectId: props.route.params.admin,
+  };
 
-  var testUser = '';
+  var toPatient = {
+    __type: 'Pointer',
+    className: 'Patient',
+    objectId: props.route.params.item.id
+  }
+
+  parseQuery.equalTo('fromAdmin', currentUser);
 
   const [messages, setMessages] = useState([]);
 
   const { isLive, isLoading, isSync, results, count, erro, reload } =
     useParseQuery(parseQuery);
 
-  useEffect(() => {
-    var mounted = false;
-    if (!mounted) {
-      currentUser = Parse.User.current();
-      console.log(currentUser);
-      testUser = currentUser;
+  function teste(object) {
+    if(object.get('from') === '1') {
+      return 1; 
     }
-    if (currentUser !== testUser) {
-      console.log('erro');
-    }
-    return () => {
-      mounted = true;
-    };
-  }, []);
+    return 2;  
+  }
 
   const onSend = useCallback((messages = []) => {
     setMessages((previousMessages) =>
@@ -42,6 +42,8 @@ export function Chat() {
     const Message = new Parse.Object('Chat');
 
     Message.set('fromAdmin', currentUser);
+    Message.set('fromPatient', toPatient);
+    Message.set('from', 1);
     Message.set('content', messages[0].text);
     Message.save();
   }, []);
@@ -55,8 +57,8 @@ export function Chat() {
           text: liveMessage.get('content'),
           createdAt: liveMessage.get('createdAt'),
           user: {
-            _id: 2,
-            name: 'React Native',
+            _id: teste(liveMessage),
+            name: 'Teste',
             avatar: 'https://placeimg.com/140/140/any',
           },
         }))
