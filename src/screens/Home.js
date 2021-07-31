a2a3ce13afd1e39c2f2896215669d215a8e0b716
YaghoMattos <mattosyagho@gmail.com>
2021-07-30 23:33:02 -0300

    feature: update home to get current user

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,3 @@
-import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaView, StatusBar, Text, View } from 'react-native';
 
@@ -7,28 +6,37 @@ import { ToChat } from '../routes/ToChat';
 import { ToPatientControl } from '../routes/ToPatientControl';
 import { ToListPatientSelectExercise } from '../routes/ToListPatientSelectExercise';
 import { ToUploadExercise } from '../routes/ToUploadExercise';
+import { CurrentUser } from '../components/CurrentUser';
 
 import Styles from '../components/Styles';
+import AuthContext from '../components/authContext';
 
 export function Home() {
+  const currentUser = CurrentUser();
+  const id = currentUser.id;
+  const token = currentUser.token;
+  const username = currentUser.username;
+
   return (
     <>
-      <StatusBar />
-      <SafeAreaView style={Styles.login_container}>
-        <View style={Styles.login_header}>
-          <Text style={Styles.login_header_text}>
-            <Text style={Styles.login_header_text_bold}>
-              {'AppReabilitação - '}
+      <AuthContext.Provider value={{ id: id, token: token, username: username }}>
+        <StatusBar />
+        <SafeAreaView style={Styles.login_container}>
+          <View style={Styles.login_header}>
+            <Text style={Styles.login_header_text}>
+              <Text style={Styles.login_header_text_bold}>
+                {'AppReabilitação - '}
+              </Text>
+              {'Menu'}
             </Text>
-            {'Menu'}
-          </Text>
-        </View>
-        <ToChat />
-        <ToPatientControl />
-        <ToListPatientSelectExercise />
-        <ToUploadExercise />
-        <Logout />
-      </SafeAreaView>
+          </View>
+          <ToChat />
+          <ToPatientControl />
+          <ToListPatientSelectExercise />
+          <ToUploadExercise />
+          <Logout />
+        </SafeAreaView>
+      </AuthContext.Provider>
     </>
   );
 }
