a8fdec8fe66c816bb0f4d9f76bc1d8c61b4be16e
YaghoMattos <mattosyagho@gmail.com>
2021-07-30 23:28:10 -0300

    feature: new flux to chat

--- src/routes/ToChat.js
+++ src/routes/ToChat.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import Styles from '../components/Styles';
+import AuthContext from '../components/authContext';
 
 export const ToChat = () => {
+  const { id, token, username } = useContext(AuthContext);
+  //console.log(id, token, username);
+
   const navigation = useNavigation();
+
   return (
     <View style={Styles.login_wrapper}>
       <View style={Styles.form}>
-        <TouchableOpacity onPress={() => navigation.navigate('Chat')}>
+        <TouchableOpacity onPress={() => navigation.navigate('ListPatientChat', id)}>
           <View style={Styles.button}>
             <Text style={Styles.button_label}>{'Chat'}</Text>
           </View>
