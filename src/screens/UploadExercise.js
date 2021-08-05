08e82d67f71f43bc8ec0ea9c6a692576c396b66f
YaghoMattos <mattosyagho@gmail.com>
2021-08-05 12:36:06 -0300

    feature: component upload video

--- src/screens/UploadExercise.js
+++ src/screens/UploadExercise.js
@@ -1,34 +1,17 @@
 import React, { useState } from 'react';
-import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import { createExercise } from '../components/Exercices';
 import Styles from '../components/Styles';
 
-export function UploadExercise() {
-  const [file, setFile] = useState('');
+import UploadImage from './UploadVideo';
 
-  async function handleCreate() {
-    const fileValue = file;
-    createExercise(/*fileValue*/);
-  }
+
+export function UploadExercise() {
 
   return (
-    <View style={Styles.login_wrapper}>
-      <View style={Styles.form}>
-        <TextInput
-          style={Styles.form_input}
-          value={file}
-          placeholder={'Upload'}
-          onChangeText={(text) => setFile(text)}
-          autoCapitalize={'none'}
-          keyboardType={'email-address'}
-        />
-        <TouchableOpacity onPress={() => handleCreate()}>
-          <View style={Styles.button}>
-            <Text style={Styles.button_label}>{'Upload Exercício'}</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    </View>
+    <SafeAreaView style={Styles.login_wrapper}>
+      <UploadImage />
+    </SafeAreaView>
   );
 }
