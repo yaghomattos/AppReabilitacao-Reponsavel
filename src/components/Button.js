a545168077687f6edd8d7e8ec22d3ec183eedefa
YaghoMattos <mattosyagho@gmail.com>
2021-08-07 01:28:50 -0300

    feature: button component

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+import Styles from '../components/Styles';
+
+export function Button({ title, onPress, props }) {
+  const navigation = useNavigation();
+
+  console.log(onPress, props)
+
+  return (
+    <View style={Styles.login_wrapper}>
+      <View style={Styles.form}>
+        <TouchableOpacity onPress={() => navigation.navigate(onPress, props)}>
+          <View style={Styles.button}>
+            <Text style={Styles.button_label}>{title}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
