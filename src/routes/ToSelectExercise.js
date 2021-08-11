ac52c7474141819152d87c3ee7f159776bfc9ea2
YaghoMattos <mattosyagho@gmail.com>
2021-08-10 23:02:16 -0300

    refactor: remove components "routes"

--- src/routes/ToSelectExercise.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { Button } from '../components/Button';
-
-export const ToSelectExercises = (patient) => {
-  return <Button title="+" onPress="SelectExercises" props={patient} />;
-};
