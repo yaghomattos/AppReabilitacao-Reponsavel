d7ecd8ad65b6c2d76371330b2c2dfbfc0a1d6dd0
YaghoMattos <mattosyagho@gmail.com>
2021-09-20 21:24:06 -0300

    refactor: login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,26 +1,68 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { SafeAreaView, StatusBar, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import { HandleLogin } from '../../components/Login/index';
+import Parse from 'parse/react-native';
 
 import Styles from '../../components/Styles';
+import styles from './styles';
 
-export function Login() {
+export const Login = () => {
+  const navigation = useNavigation();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const doUserLogIn = async function () {
+    const usernameValue = username;
+    const passwordValue = password;
+    return await Parse.User.logIn(usernameValue, passwordValue)
+      .then(async (loggedInUser) => {
+        Alert.alert('Login realizado com sucesso!');
+        const currentUser = await Parse.User.currentAsync();
+        console.log(loggedInUser === currentUser);
+        navigation.navigate('Home');
+      })
+      .catch((error) => {
+        Alert.alert('Error!', error.message);
+      });
+  };
+
   return (
-    <>
-      <StatusBar />
-      <SafeAreaView style={Styles.login_container}>
-        <View style={Styles.login_header}>
-          <Text style={Styles.login_header_text}>
-            <Text style={Styles.login_header_text_bold}>
-              {'AppReabilitação - '}
-            </Text>
-            {'Responsável'}
+    <View style={styles.wrapper}>
+      <View style={styles.logo}>
+        <Text style={styles.text_label}>Logo</Text>
+      </View>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          value={username}
+          placeholder={'Usuário'}
+          onChangeText={(text) => setUsername(text)}
+          autoCapitalize={'none'}
+          keyboardType={'email-address'}
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          placeholder={'Senha'}
+          secureTextEntry
+          onChangeText={(text) => setPassword(text)}
+        />
+        <TouchableOpacity onPress={() => doUserLogIn()}>
+          <View style={styles.button}>
+            <Text style={styles.text_label}>{'Entrar'}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <>
+        <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+          <Text style={styles.footer_text}>
+            {'Não possui conta? '}
+            <Text style={styles.footer_link}>{'Registre-se'}</Text>
           </Text>
-        </View>
-        <HandleLogin />
-      </SafeAreaView>
-    </>
+        </TouchableOpacity>
+      </>
+    </View>
   );
-}
+};
