15339a0e06919d785b1eeaaa13e9e0d979d7aa73
YaghoMattos <mattosyagho@gmail.com>
2021-09-24 20:35:35 -0300

    style: control patient

--- src/screens/PatientControl/index.js
+++ src/screens/PatientControl/index.js
@@ -2,24 +2,37 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaView, StatusBar, Text, View } from 'react-native';
 
+import { useNavigation } from '@react-navigation/core';
+
 import { Button } from '../../components/Button/index';
 
 import Styles from '../../components/Styles';
+import styles from './styles';
+import { Ionicons } from '@expo/vector-icons';
 
 export function PatientControl() {
+  const navigation = useNavigation();
+
   return (
     <>
       <StatusBar />
-      <SafeAreaView style={Styles.login_container}>
-        <View style={Styles.login_header}>
-          <Text style={Styles.login_header_text}>
-            <Text style={Styles.login_header_text_bold}>{'AppReabilitação - '}</Text>
-            {'Cadastro Paciente'}
-          </Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <Ionicons
+              name="arrow-back"
+              size={24}
+              style={styles.back}
+              onPress={() => navigation.goBack()}
+            />
+          <Text style={styles.header_text}>{'Cadastro de Paciente'}</Text>
         </View>
-        <Button title="Novo Paciente" onPress="PatientRecord" />
-        <Button title="Deletar Paciente" onPress="DeletePatient" />
-        <Button title="Lista de Pacientes" onPress="ListPatients" />
+        <View style={styles.background}>
+          <View style={styles.buttons}>
+            <Button title="Novo Paciente" onPress="PatientRecord" />
+            <Button title="Deletar Paciente" onPress="DeletePatient" />
+            <Button title="Lista de Pacientes" onPress="ListPatients" />
+          </View>
+        </View>
       </SafeAreaView>
     </>
   );
---
+++ src/screens/PatientControl/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3E9ACD',
+  },
+  header: {
+    justifyContent: 'space-evenly',
+    height: '30%',
+  },
+  back: {
+    paddingLeft: 20,
+    color: '#fff',
+  },
+  header_text: {
+    color: '#fff',
+    fontSize: 18,
+    paddingLeft: '10%',
+    fontWeight: 'bold',
+  },
+  background: {
+    height: '100%',
+    borderTopStartRadius: 20,
+    borderTopEndRadius: 20,
+    backgroundColor: '#f6f6f6'
+  },
+  buttons: {
+    height: '40%',
+  },
+});
+
+export default styles;
