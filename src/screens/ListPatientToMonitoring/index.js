31dc781f3e843e633b02f5a18cb54705d30f96fb
YaghoMattos <mattosyagho@gmail.com>
2021-09-30 17:21:07 -0300

    feature: list to monitoring patient

---
+++ src/screens/ListPatientToMonitoring/index.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList, TextInput } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+import { List, Divider } from 'react-native-paper';
+import { useParseQuery } from '@parse/react-native';
+
+import Parse from 'parse/react-native.js';
+
+import Styles from '../../components/Styles';
+import styles from './styles';
+import { Ionicons } from '@expo/vector-icons';
+
+const parseQuery = new Parse.Query('Patient');
+parseQuery.ascending('name');
+
+export const ListPatientMonitoring = () => {
+  const [search, setSearch] = useState('');
+
+  const results = useParseQuery(parseQuery).results;
+
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <View style={styles.header}>
+        <View style={styles.backView}>
+          <Ionicons
+            name="arrow-back"
+            size={24}
+            style={styles.back}
+            onPress={() => navigation.goBack()}
+          />
+        </View>
+        <TextInput
+          style={styles.input}
+          value={search}
+          placeholder={'Pesquisar'}
+          onChangeText={(text) => setSearch(text)}
+          autoCapitalize={'none'}
+          keyboardType={'email-address'}
+        />
+      </View>
+      <View style={styles.viewList}>
+        <FlatList
+          numColumns={2}
+          data={results}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={() => <Divider />}
+          renderItem={({ item }) => (
+            <List.Item
+              style={styles.item}
+              title={item.get('name')}
+              titleNumberOfLines={1}
+              titleStyle={styles.itemTitle}
+              onPress={() =>
+                navigation.navigate('Monitoring', item.id)
+              }
+            />
+          )}
+        />
+      </View>
+    </>
+  );
+};
