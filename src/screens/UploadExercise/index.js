4cbfd2d748a5208031a85824f010a4fc7a377cd0
YaghoMattos <mattosyagho@gmail.com>
2021-10-05 21:52:07 -0300

    refactor: upload exercise

--- src/screens/UploadExercise/index.js
+++ src/screens/UploadExercise/index.js
@@ -1,14 +1,75 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-
-import { UploadVideo } from '../../components/UploadVideo/index';
-
-import Styles from '../../components/Styles';
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export function UploadExercise() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
+  async function upload() {
+    const myfile = image;
+    const parseFile = new Parse.File('image', { base64: myfile.base64 });
+    try {
+      const Gallery = Parse.Object.extend('Exercise');
+      const gallery = new Gallery();
+      gallery.set('video', parseFile);
+      gallery.set('name', 'nome');
+      gallery.set('description', 'descrição');
+
+      gallery.save();
+      Alert.alert('Arquivo salvo na Back4app.');
+    } catch (error) {
+      console.log(
+        'The file either could not be read, or could not be saved to Back4app.'
+      );
+    }
+  }
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+      base64: true,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result);
+    }
+  };
+
   return (
-    <SafeAreaView style={Styles.login_wrapper}>
-      < UploadVideo />
-    </SafeAreaView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Selecionar imagem da galeria" onPress={pickImage} />
+      {image && (
+        <Image
+          source={{ uri: image.uri }}
+          style={{
+            width: 250,
+            height: 250,
+            marginTop: 50,
+            marginBottom: 50,
+            resizeMode: 'cover',
+            alignSelf: 'center',
+          }}
+        />
+      )}
+
+      {image && <Button title="Upload" color="green" onPress={upload} />}
+    </View>
   );
 }
