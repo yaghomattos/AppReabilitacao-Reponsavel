8e92027b7c8aafb7c48b892df35ebd3ed6fa5427
YaghoMattos <mattosyagho@gmail.com>
2021-10-15 16:58:24 -0300

    feature: upload exam

---
+++ src/screens/UploadExam/index.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Platform } from 'react-native';
+
+import * as ImagePicker from 'expo-image-picker';
+
+import { createExam } from '../../components/Exams';
+
+export function UploadExam() {
+  const [file, setFile] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
+  async function upload() {
+    var exam = {
+      video: file,
+      name: 'nome',
+      description: 'descrição',
+    };
+
+    createExam(exam);
+  }
+
+  const pickFile = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      base64: true,
+    });
+
+    if (!result.cancelled) {
+      setFile(result);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Selecionar arquivo da galeria" onPress={pickFile} />
+      {file && (
+        <Image
+          source={{ uri: file.uri }}
+          style={{
+            width: 250,
+            height: 250,
+            marginTop: 50,
+            marginBottom: 50,
+            resizeMode: 'cover',
+            alignSelf: 'center',
+          }}
+        />
+      )}
+
+      {file && <Button title="Upload" color="green" onPress={upload} />}
+    </View>
+  );
+}
