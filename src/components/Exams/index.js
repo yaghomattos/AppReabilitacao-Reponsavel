e2e5aa90d1c42a7003fd4b8d4c1304f2a07a83c8
YaghoMattos <mattosyagho@gmail.com>
2021-10-16 23:17:32 -0300

    fix: params in exams crud

--- src/components/Exams/index.js
+++ src/components/Exams/index.js
@@ -2,16 +2,25 @@ import Parse from 'parse/react-native.js';
 import { Alert } from 'react-native';
 
 export async function createExam(props) {
-  var { base64, fileName } = props.video;
-  fileName = props.name;
-  const parseFile = new Parse.File(fileName, { base64 });
+  var { base64, examName } = props.video;
+  examName = props.name + '.gif';
+  const parseFileExam = new Parse.File(examName, { base64 });
+
+  var { base64, photoName } = props.photo;
+  photoName = props.name + '.png';
+  const parseFilePhoto = new Parse.File(photoName, { base64 });
+
   try {
-    const responseFile = await parseFile.save();
+    const examFile = await parseFileExam.save();
+    const photoFile = await parseFilePhoto.save();
+
     const File = Parse.Object.extend('Exam');
     const object = new File();
-    object.set('video', responseFile);
+    object.set('video', examFile);
+    object.set('photo', photoFile);
     object.set('name', props.name);
     object.set('description', props.description);
+    object.set('timer', props.timer);
 
     await object.save();
     Alert.alert('Exame Salvo');
