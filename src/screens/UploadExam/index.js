6163b2f91679016c3b017d949dae3e27d52e81ac
YaghoMattos <mattosyagho@gmail.com>
2021-10-16 23:18:58 -0300

    feature: update upload exam

--- src/screens/UploadExam/index.js
+++ src/screens/UploadExam/index.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
-
+import {
+  Image,
+  View,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
 
 import { createExam } from '../../components/Exams';
 
+import { Ionicons } from '@expo/vector-icons';
+
+import styles from './styles';
+
 export function UploadExam() {
+  const navigation = useNavigation();
+
   const [file, setFile] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [timer, setTimer] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -23,8 +41,10 @@ export function UploadExam() {
   async function upload() {
     var exam = {
       video: file,
+      photo: photo,
       name: 'nome',
       description: 'descrição',
+      timer: '60',
     };
 
     createExam(exam);
@@ -41,24 +61,83 @@ export function UploadExam() {
     }
   };
 
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Selecionar arquivo da galeria" onPress={pickFile} />
-      {file && (
-        <Image
-          source={{ uri: file.uri }}
-          style={{
-            width: 250,
-            height: 250,
-            marginTop: 50,
-            marginBottom: 50,
-            resizeMode: 'cover',
-            alignSelf: 'center',
-          }}
-        />
-      )}
+  const pickPhoto = async () => {
+    let resultPhoto = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      base64: true,
+    });
 
-      {file && <Button title="Upload" color="green" onPress={upload} />}
+    if (!resultPhoto.cancelled) {
+      setPhoto(resultPhoto);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.backView}>
+          <Ionicons
+            name="arrow-back"
+            size={24}
+            style={styles.back}
+            onPress={() => navigation.goBack()}
+          />
+          <Text style={styles.header_text}>{'Cadastrar Exame'}</Text>
+        </View>
+      </View>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.background}>
+          <TextInput
+            style={styles.input}
+            value={name}
+            placeholder={'nome do exame'}
+            onChangeText={(text) => setName(text)}
+            autoCapitalize={'none'}
+            keyboardType={'email-address'}
+          />
+          <TextInput
+            editable
+            multiline
+            style={styles.input}
+            value={description}
+            placeholder={'descrição do exame'}
+            onChangeText={(text) => setDescription(text)}
+            autoCapitalize={'none'}
+            keyboardType={'email-address'}
+          />
+          <TextInput
+            style={styles.input}
+            value={timer}
+            placeholder={'tempo em segundos'}
+            onChangeText={(text) => setTimer(text)}
+            autoCapitalize={'none'}
+            keyboardType={'numeric'}
+          />
+          <View style={styles.preview}>
+            <TouchableOpacity
+              style={file ? styles.button : styles.button2}
+              onPress={pickFile}
+            >
+              <Text style={styles.text_label}>{'Selecionar Exame'}</Text>
+            </TouchableOpacity>
+            {file && <Image source={{ uri: file.uri }} style={styles.image} />}
+          </View>
+          <View style={styles.preview}>
+            <TouchableOpacity style={styles.button} onPress={pickPhoto}>
+              <Text style={styles.text_label}>{'Selecionar Foto'}</Text>
+            </TouchableOpacity>
+            {photo && (
+              <Image source={{ uri: photo.uri }} style={styles.image} />
+            )}
+          </View>
+
+          {file && photo && (
+            <TouchableOpacity style={styles.send} onPress={upload}>
+              <Text style={styles.text_label}>{'Cadastrar'}</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 }
