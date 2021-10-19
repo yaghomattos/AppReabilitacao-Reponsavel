f2a3b0d789f9c21eae7097d579f3f7bdfbac23ca
YaghoMattos <mattosyagho@gmail.com>
2021-10-19 11:01:45 -0300

    fix: search form in view form

--- src/screens/ViewForm/index.js
+++ src/screens/ViewForm/index.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 
+import { readForm } from '../../components/Form';
+
 import { Ionicons } from '@expo/vector-icons';
 
 import styles from './styles';
@@ -9,11 +11,28 @@ import styles from './styles';
 export function ViewForm(props) {
   const navigation = useNavigation();
 
-  const form = props.route.params;
+  const [reps, setReps] = useState('');
+  const [frequency, setFrequency] = useState('');
+  const [saturation, setSaturation] = useState('');
+  const [dyspnea, setDyspnea] = useState('');
+  const [fatigue, setFatigue] = useState('');
+
+  const formId = props.route.params.id;
+
+  async function update() {
+    readForm(formId).then((response) => {
+      setReps(response.get('Reps'));
+      setFrequency(response.get('Frequency'));
+      setSaturation(response.get('Saturation'));
+      setDyspnea(response.get('Dyspnea'));
+      setFatigue(response.get('Fatique'));
+    });
+  }
+
+  update();
 
   var exam = false;
-
-  if (form.get('reps') != undefined && form.get('reps') != '') exam = true;
+  if (reps != undefined && reps != '') exam = true;
 
   return (
     <View style={styles.wrapper}>
@@ -33,19 +52,19 @@ export function ViewForm(props) {
           <Text style={styles.title}>
             {exam ? 'Número de repetições' : null}
           </Text>
-          <Text style={styles.label}>{exam ? form.get('reps') : null}</Text>
+          <Text style={styles.label}>{exam ? reps : null}</Text>
 
           <Text style={styles.title}>{'Frequência Cardíaca'}</Text>
-          <Text style={styles.label}>{form.get('Frequency')}</Text>
+          <Text style={styles.label}>{frequency}</Text>
 
           <Text style={styles.title}>{'Saturação'}</Text>
-          <Text style={styles.label}>{form.get('Saturation')}</Text>
+          <Text style={styles.label}>{saturation}</Text>
 
           <Text style={styles.title}>{'Borg Dispineia'}</Text>
-          <Text style={styles.label}>{form.get('Dyspnea')}</Text>
+          <Text style={styles.label}>{dyspnea}</Text>
 
           <Text style={styles.title}>{'Borg Fadiga'}</Text>
-          <Text style={styles.label}>{form.get('Fatique')}</Text>
+          <Text style={styles.label}>{fatigue}</Text>
 
           <TouchableOpacity onPress={() => navigation.goBack()}>
             <View style={styles.button}>
