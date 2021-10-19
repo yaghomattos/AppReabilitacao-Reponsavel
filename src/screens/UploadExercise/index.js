e1b034a4e0e4ac14b9e7c452ac53b0c161010eff
YaghoMattos <mattosyagho@gmail.com>
2021-10-18 21:59:43 -0300

    feature: new params upload exercise

--- src/screens/UploadExercise/index.js
+++ src/screens/UploadExercise/index.js
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
-
+import {
+  Image,
+  View,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
 
 import { createExercise } from '../../components/Exercises';
 
+import { Ionicons } from '@expo/vector-icons';
+
+import styles from './styles';
+
 export function UploadExercise() {
+  const navigation = useNavigation();
+
   const [file, setFile] = useState(null);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -23,8 +39,8 @@ export function UploadExercise() {
   async function upload() {
     var exercise = {
       video: file,
-      name: 'nome',
-      description: 'descrição',
+      name: name,
+      description: description,
     };
 
     createExercise(exercise);
@@ -42,23 +58,55 @@ export function UploadExercise() {
   };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Selecionar arquivo da galeria" onPress={pickFile} />
-      {file && (
-        <Image
-          source={{ uri: file.uri }}
-          style={{
-            width: 250,
-            height: 250,
-            marginTop: 50,
-            marginBottom: 50,
-            resizeMode: 'cover',
-            alignSelf: 'center',
-          }}
-        />
-      )}
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.backView}>
+          <Ionicons
+            name="arrow-back"
+            size={24}
+            style={styles.back}
+            onPress={() => navigation.goBack()}
+          />
+          <Text style={styles.header_text}>{'Cadastrar Exercício'}</Text>
+        </View>
+      </View>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.background}>
+          <TextInput
+            style={styles.input}
+            value={name}
+            placeholder={'nome do exercício'}
+            onChangeText={(text) => setName(text)}
+            autoCapitalize={'none'}
+            keyboardType={'email-address'}
+          />
+          <TextInput
+            editable
+            multiline
+            style={styles.input}
+            value={description}
+            placeholder={'descrição do exercício'}
+            onChangeText={(text) => setDescription(text)}
+            autoCapitalize={'none'}
+            keyboardType={'email-address'}
+          />
+          <View style={styles.preview}>
+            <TouchableOpacity
+              style={file ? styles.button : styles.button2}
+              onPress={pickFile}
+            >
+              <Text style={styles.text_label}>{'Selecionar Exercício'}</Text>
+            </TouchableOpacity>
+            {file && <Image source={{ uri: file.uri }} style={styles.image} />}
+          </View>
 
-      {file && <Button title="Upload" color="green" onPress={upload} />}
+          {file && (
+            <TouchableOpacity style={styles.send} onPress={upload}>
+              <Text style={styles.text_label}>{'Cadastrar'}</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 }
