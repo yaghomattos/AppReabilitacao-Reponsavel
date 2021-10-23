5e4772e2b6ad6328147776cb7ef74ae84ae6a010
YaghoMattos <mattosyagho@gmail.com>
2021-10-23 16:04:56 -0300

    styles: exam settings

---
+++ src/screens/ExamSettings/styles.js
@@ -0,0 +1,60 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#3E9ACD',
+    alignItems: 'center',
+    height: "20%",
+  },
+  backView: {
+    width: '100%', 
+    paddingStart: 20,
+    margin: 30,
+  },
+  back: {
+    color: '#fff',
+  },
+  header_text: {
+    color: '#fff',
+    fontSize: 18,
+    paddingLeft: '10%',
+    paddingTop: '4%',
+    fontWeight: 'bold',
+  },
+  container: {
+    flex: 1,
+    paddingBottom: 50,
+    justifyContent: 'center',
+    backgroundColor: '#3E9ACD',
+  },
+  form: {
+    alignItems: 'center',
+  },
+  inputName: {
+    color: '#fff',
+    fontSize: 15,
+  },
+  input: {
+    width: "40%",
+    height: 40,
+    paddingHorizontal: 20,
+    marginBottom: "5%",
+    backgroundColor: '#F4F4F4',
+    borderRadius: 10,
+  },
+  button: {
+    width: 100,
+    height: 50,
+    marginTop: '5%',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#384955',
+  },
+  text_label: {
+    color: '#fff',
+    fontSize: 15,
+  },
+});
+
+export default styles;
