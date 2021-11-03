e8029fa4b2664c39d226f5de0431401d9b56ecf2
YaghoMattos <mattosyagho@gmail.com>
2021-11-02 23:12:56 -0300

    feature: menu to orientation resources

---
+++ src/screens/MenuOrientation/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+
+import { Button } from '../../components/Button/index';
+
+import { Ionicons } from '@expo/vector-icons';
+
+import styles from './styles';
+
+export function MenuOrientation(props) {
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <StatusBar />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <Ionicons
+            name="arrow-back"
+            size={24}
+            style={styles.back}
+            onPress={() => navigation.goBack()}
+          />
+          <Text style={styles.header_text}>{'Orientações'}</Text>
+        </View>
+        <View style={styles.background}>
+          <View style={styles.buttons}>
+            <Button
+              title="Orientações selecionadas"
+              onPress="ListPatientSelectOrientation"
+            />
+            <Button
+              title="Cadastrar nova Orientação"
+              onPress="NewOrientation"
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
