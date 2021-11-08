90f6d07d1be61dc370242314a2bdecd7e6d6e63d
YaghoMattos <mattosyagho@gmail.com>
2021-11-08 14:41:40 -0300

    feature: list patient to educational

---
+++ src/screens/ListPatientToEducational/index.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { View, FlatList, Text, TextInput } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { List, Divider } from 'react-native-paper';
+import { useParseQuery } from '@parse/react-native';
+import Parse from 'parse/react-native.js';
+
+import { Ionicons } from '@expo/vector-icons';
+
+import styles from './styles';
+
+const parseQuery = new Parse.Query('Patient');
+parseQuery.ascending('name');
+
+export const ListPatientEducational = (props) => {
+  const navigation = useNavigation();
+
+  const [search, setSearch] = useState('');
+
+  const results = useParseQuery(parseQuery).results;
+
+  Parse.User._clearCache();
+
+  const adminId = props.route.params;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.backView}>
+          <Ionicons
+            name="arrow-back"
+            size={24}
+            style={styles.back}
+            onPress={() => navigation.goBack()}
+          />
+        </View>
+        <TextInput
+          style={styles.input}
+          value={search}
+          placeholder={'Pesquisar'}
+          onChangeText={(text) => setSearch(text)}
+          autoCapitalize={'none'}
+          keyboardType={'email-address'}
+        />
+      </View>
+      <View style={styles.viewList}>
+        <FlatList
+          numColumns={1}
+          data={results}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={() => <Divider />}
+          renderItem={({ item }) => (
+            <List.Item
+              style={styles.item}
+              title={item.get('name')}
+              titleNumberOfLines={1}
+              titleStyle={styles.itemTitle}
+              onPress={() =>
+                navigation.navigate('Educational', { item, adminId })
+              }
+            />
+          )}
+        />
+      </View>
+    </View>
+  );
+};
