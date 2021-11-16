c3dcf19c7b19d74ad085db0a7e057726c32d9253
YaghoMattos <mattosyagho@gmail.com>
2021-11-15 23:04:01 -0300

    feature: checkbox and update timer in exam setting

--- src/screens/ExamSettings/index.js
+++ src/screens/ExamSettings/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
-import Parse from 'parse/react-native.js';
+import { Checkbox } from 'react-native-paper';
 
 import { updateSelectExams } from '../../components/SelectExams';
 
@@ -12,17 +12,36 @@ import styles from './styles';
 export function ExamSettings(props) {
   const navigation = useNavigation();
 
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState('');
+  const [seconds, setSeconds] = useState('');
+
+  const [reps, setReps] = useState(false);
+  const [frequency, setFrequency] = useState(false);
+  const [saturation, setSaturation] = useState(false);
+  const [dyspnea, setDyspnea] = useState(false);
+  const [fatigue, setFatigue] = useState(false);
 
   var examId = props.route.params;
 
   async function handleSettings() {
-    updateSelectExams(examId, parseInt(minutes) * 60 + parseInt(seconds));
+    if (minutes == '') setMinutes('');
+    if (seconds == '') setSeconds('');
+
+    const object = {
+      examId: examId,
+      timer: parseInt(minutes) * 60 + parseInt(seconds),
+      reps: reps,
+      frequency: frequency,
+      saturation: saturation,
+      dyspnea: dyspnea,
+      fatigue: fatigue,
+    };
+
+    updateSelectExams(object);
   }
 
   return (
-    <>
+    <View style={styles.wrapper}>
       <View style={styles.header}>
         <View style={styles.backView}>
           <Ionicons
@@ -35,31 +54,81 @@ export function ExamSettings(props) {
         </View>
       </View>
       <View style={styles.container}>
+        <Text style={styles.title}>{'Cronômetro'}</Text>
         <View style={styles.form}>
-          <Text style={styles.inputName}>{'Cronômetro'}</Text>
           <TextInput
             style={styles.input}
             value={minutes}
-            placeholder={'minutos'}
-            onChangeText={(text) => setMinutes(text)}
+            placeholder={'min'}
+            onChangeText={(text) => setMinutes(parseInt(text))}
             keyboardType={'numeric'}
             maxLength={2}
           />
+          <Text style={styles.inputText}>{':'}</Text>
           <TextInput
             style={styles.input}
             value={seconds}
-            placeholder={'segundos'}
-            onChangeText={(text) => setSeconds(text)}
+            placeholder={'seg'}
+            onChangeText={(text) => setSeconds(parseInt(text))}
             keyboardType={'numeric'}
-            maxLength={4}
+            maxLength={3}
           />
+        </View>
+        <View style={styles.wrapperCheckbox}>
+          <View style={styles.checkboxContainer}>
+            <Checkbox
+              status={reps ? 'checked' : 'unchecked'}
+              onPress={() => {
+                setReps(!reps);
+              }}
+            />
+            <Text style={styles.text_checkbox}>Número de repetições</Text>
+          </View>
+          <View style={styles.checkboxContainer}>
+            <Checkbox
+              status={frequency ? 'checked' : 'unchecked'}
+              onPress={() => {
+                setFrequency(!frequency);
+              }}
+            />
+            <Text style={styles.text_checkbox}>Frequência Cardíaca</Text>
+          </View>
+          <View style={styles.checkboxContainer}>
+            <Checkbox
+              status={saturation ? 'checked' : 'unchecked'}
+              onPress={() => {
+                setSaturation(!saturation);
+              }}
+            />
+            <Text style={styles.text_checkbox}>Saturação</Text>
+          </View>
+          <View style={styles.checkboxContainer}>
+            <Checkbox
+              status={dyspnea ? 'checked' : 'unchecked'}
+              onPress={() => {
+                setDyspnea(!dyspnea);
+              }}
+            />
+            <Text style={styles.text_checkbox}>Falta de ar</Text>
+          </View>
+          <View style={styles.checkboxContainer}>
+            <Checkbox
+              status={fatigue ? 'checked' : 'unchecked'}
+              onPress={() => {
+                setFatigue(!fatigue);
+              }}
+            />
+            <Text style={styles.text_checkbox}>Cansaço</Text>
+          </View>
+        </View>
+        {(minutes != '' || seconds != '') && (
           <TouchableOpacity onPress={() => handleSettings()}>
             <View style={styles.button}>
               <Text style={styles.text_label}>{'Salvar'}</Text>
             </View>
           </TouchableOpacity>
-        </View>
+        )}
       </View>
-    </>
+    </View>
   );
 }
