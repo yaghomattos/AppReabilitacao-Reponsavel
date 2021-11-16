7f498111a00ddb5d7de938286b2f066e955160f2
YaghoMattos <mattosyagho@gmail.com>
2021-11-16 16:04:39 -0300

    styles: button component update

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -9,13 +9,11 @@ export function Button({ title, onPress, props }) {
 
   return (
     <View style={styles.wrapper}>
-      <View style={styles.form}>
-        <TouchableOpacity onPress={() => navigation.navigate(onPress, props)}>
-          <View style={styles.button}>
-            <Text style={styles.button_label}>{title}</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
+      <TouchableOpacity onPress={() => navigation.navigate(onPress, props)}>
+        <View style={styles.button}>
+          <Text style={styles.button_label}>{title}</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -2,22 +2,17 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   wrapper: {
-    flex: 0.5,
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 20,
     borderRadius: 10,
-    backgroundColor: '#f5f5f5',
-  },
-  form: {
-    width: '54%',
-    maxWidth: 280,
   },
   button: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     height: 40,
+    width: 200,
     borderRadius: 10,
     backgroundColor: '#384955',
   },
