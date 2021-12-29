8230d4b65a6487f36937a2c816b61c783ed60b26
Yagho Mattos <mattosyagho@gmail.com>
2021-12-29 20:09:41 -0300

    feature: correct dotenv

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,22 +1,35 @@
+import {
+  REACT_APP_API_KEY,
+  REACT_APP_APP_ID,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_MEASUREMENT_ID,
+  REACT_APP_MENSSAGING_SENDER_ID,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+} from '@env';
 import firebase from 'firebase';
-
 import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.NATIVE_APP_API_KEY,
-  authDomain: process.env.NATIVE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.NATIVE_APP_DATABASE_URL,
-  projectId: process.env.NATIVE_APP_PROJECT_ID,
-  storageBucket: process.env.NATIVE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.NATIVE_APP_MENSSAGING_SENDER_ID,
-  appId: process.env.NATIVE_APP_APP_ID,
-  measurementId: process.env.NATIVE_APP_MEASUREMENT_ID,
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MENSSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
 
-//const auth = firebase.auth();
+const auth = firebase.auth();
 const database = firebase.database();
 
-export { firebase, database };
+export { firebase, database, auth };
