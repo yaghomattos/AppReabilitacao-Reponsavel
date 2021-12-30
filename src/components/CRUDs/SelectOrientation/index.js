269ecdec856164e01a226d89e254fcc1d1ee815e
Yagho Mattos <mattosyagho@gmail.com>
2021-12-30 12:51:52 -0300

    feature: select orientation firebase crud

--- src/components/CRUDs/SelectOrientation/index.js
+++ src/components/CRUDs/SelectOrientation/index.js
@@ -1,80 +1,36 @@
-import Parse from 'parse/react-native.js';
 import { Alert } from 'react-native';
 
 export async function createSelectOrientations(props) {
-  var exercisePointer = {
-    __type: 'Pointer',
-    className: 'Exercise',
-    objectId: props.testOrExerciseId,
-  };
-  var testPointer = {
-    __type: 'Pointer',
-    className: 'Test',
-    objectId: props.testOrExerciseId,
-  };
-  var orientationPointer = {
-    __type: 'Pointer',
-    className: 'Orientation',
-    objectId: props.orientationId,
-  };
+  const selectOrientationRef = database.ref('selectOrientation');
 
-  const myNewObject = new Parse.Object('SelectOrientations');
-  if (props.className == 'test') myNewObject.set('test', testPointer);
-  else myNewObject.set('exercise', exercisePointer);
-  myNewObject.set('orientation', orientationPointer);
-  try {
-    const result = await myNewObject.save();
-    if (props.className == 'test')
-      Alert.alert('Orientação selecionada para o teste');
-    else Alert.alert('Orientação selecionada para o exercício');
-  } catch (error) {
-    console.error('Error while creating SelectOrientations: ', error);
-  }
-}
-
-export async function readSelectOrientations() {
-  const SelectOrientations = Parse.Object.extend('SelectOrientations');
-  const query = new Parse.Query(SelectOrientations);
-  try {
-    const results = await query.find();
-  } catch (error) {
-    console.error('Error while fetching SelectOrientations', error);
-  }
-}
-
-export async function updateSelectOrientations(orientationId, text) {
-  var orientationPointer = {
-    __type: 'Pointer',
-    className: 'Orientation',
-    objectId: orientationId,
-  };
-
-  const query = new Parse.Query('SelectOrientations');
-  try {
-    const object = await query.get(orientationPointer);
-    if (text != 0) object.set('text', text);
-    try {
-      const response = await object.save();
-      console.log('SelectOrientations updated', response);
-    } catch (error) {
-      console.error('Error while updating SelectOrientations', error);
-    }
-  } catch (error) {
-    console.error('Error while retrieving object SelectOrientations', error);
+  if (props.className == 'exercise') {
+    selectOrientationRef
+      .push({
+        orientation: props.orientationId,
+        exercise: props.testOrExerciseId,
+      })
+      .then(() => {
+        Alert.alert('Orientação selecionada para o exercício');
+      })
+      .catch(() => {
+        Alert.alert('Erro ao selecionar orientação');
+      });
+  } else {
+    selectOrientationRef
+      .push({
+        orientation: props.orientationId,
+        test: props.testOrExerciseId,
+      })
+      .then(() => {
+        Alert.alert('Orientação selecionada para o teste');
+      })
+      .catch(() => {
+        Alert.alert('Erro ao selecionar orientação');
+      });
   }
 }
 
 export async function deleteSelectOrientations(props) {
-  const query = new Parse.Query('SelectOrientations');
-  try {
-    const object = await query.get(props);
-    try {
-      const response = await object.destroy();
-      console.log('Deleted SelectOrienatitons', response);
-    } catch (error) {
-      console.error('Error while deleting SelectOrienatitons', error);
-    }
-  } catch (error) {
-    console.error('Error while retrieving SelectOrienatitons', error);
-  }
+  const selectOrientationRef = database.ref('selectOrientation/' + props);
+  selectOrientationRef.remove();
 }
