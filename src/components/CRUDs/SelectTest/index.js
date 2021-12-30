595d64ca37083fc56792213632e78f864e167a72
Yagho Mattos <mattosyagho@gmail.com>
2021-12-30 12:51:40 -0300

    feature: select test firebase crud

--- src/components/CRUDs/SelectTest/index.js
+++ src/components/CRUDs/SelectTest/index.js
@@ -1,85 +1,57 @@
-import Parse from 'parse/react-native.js';
 import { Alert } from 'react-native';
 
-export async function createSelectTests(participantId, testId) {
-  var participantPointer = {
-    __type: 'Pointer',
-    className: 'Participant',
-    objectId: participantId,
-  };
-  var testPointer = {
-    __type: 'Pointer',
-    className: 'Test',
-    objectId: testId,
-  };
+export async function createSelectTests(props) {
+  const selectTestRef = database.ref('selectTest');
 
-  const myNewObject = new Parse.Object('SelectTests');
-  myNewObject.set('participant', participantPointer);
-  myNewObject.set('test', testPointer);
-  try {
-    const result = await myNewObject.save();
-    Alert.alert('Teste selecionado para o paciente');
-  } catch (error) {
-    console.error('Error while creating SelectTests: ', error);
-  }
-}
-
-export async function readSelectTests() {
-  const SelectTests = Parse.Object.extend('SelectTests');
-  const query = new Parse.Query(SelectTests);
-  try {
-    const results = await query.find();
-    for (const object of results) {
-      const Participant = object.get('Participant');
-      const test = object.get('test');
-      console.log(Participant);
-      console.log(test);
-    }
-  } catch (error) {
-    console.error('Error while fetching SelectTests', error);
-  }
+  selectTestRef
+    .push({
+      participant: props.participant,
+      test: props.test,
+    })
+    .then(() => {
+      Alert.alert('Teste selecionado');
+    })
+    .catch(() => {
+      Alert.alert('Erro ao selecionar teste');
+    });
 }
 
 export async function updateSelectTests(props) {
-  var testPointer = {
-    __type: 'Pointer',
-    className: 'Test',
-    objectId: props.testId,
-  };
+  const selectTestRef = database.ref('selectTest/' + props.id);
 
-  const query = new Parse.Query('SelectTests');
-  try {
-    const object = await query.get(testPointer);
-    if (props.timer != '') object.set('timer', props.timer);
-    else if (props.numReps != '') object.set('numReps', props.numReps);
+  if (props.timer != '')
+    selectTestRef.update({
+      timer: props.timer,
+    });
+  else
+    selectTestRef.update({
+      numReps: props.numReps,
+    });
 
-    if (props.reps) object.set('reps', true);
-    if (props.frequency) object.set('frequency', true);
-    if (props.saturation) object.set('saturation');
-    if (props.dyspnea) object.set('dyspnea', true);
-    if (props.fatigue) object.set('fatigue', true);
-    try {
-      const response = await object.save();
-      console.log('SelectTests updated', response);
-    } catch (error) {
-      console.error('Error while updating SelectTests', error);
-    }
-  } catch (error) {
-    console.error('Error while retrieving object SelectTests', error);
-  }
+  if (props.reps)
+    selectTestRef.update({
+      reps: true,
+    });
+  if (props.frequency)
+    selectTestRef.update({
+      frequency: true,
+    });
+  if (props.saturation)
+    selectTestRef.update({
+      saturation: true,
+    });
+  if (props.dyspnea)
+    selectTestRef.update({
+      dyspnea: true,
+    });
+  if (props.fatique)
+    selectTestRef.update({
+      fatique: true,
+    });
+  console.log('test updated');
 }
 
-export async function deleteSelectTests() {
-  const query = new Parse.Query('SelectTests');
-  try {
-    const object = await query.get('xKue915KBG');
-    try {
-      const response = await object.destroy();
-      console.log('Deleted ParseObject', response);
-    } catch (error) {
-      console.error('Error while deleting ParseObject', error);
-    }
-  } catch (error) {
-    console.error('Error while retrieving ParseObject', error);
-  }
+export async function deleteSelectTests(props) {
+  const selectTestRef = database.ref('selectTest/' + props);
+  selectTestRef.remove();
 }
