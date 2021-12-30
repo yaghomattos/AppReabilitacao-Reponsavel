3de08adca5ef4b207a4fa56c5a456cc383ba91c5
Yagho Mattos <mattosyagho@gmail.com>
2021-12-29 22:09:37 -0300

    feature: form firebase crud

--- src/components/CRUDs/Form/index.js
+++ src/components/CRUDs/Form/index.js
@@ -1,33 +1,10 @@
-import Parse from 'parse/react-native.js';
+export async function readForm(props) {
+  const formRef = await database.ref(`form/${props}`).get();
 
-export async function createForm(freq, sat, dysp, fat) {
-  const myNewObject = new Parse.Object('Form');
-  myNewObject.set('Frequency', freq);
-  myNewObject.set('Saturation', sat);
-  myNewObject.set('Dyspnea', dysp);
-  myNewObject.set('Fatique', fat);
-  try {
-    const result = await myNewObject.save();
-    if (result !== undefined) return result.id;
-    return false;
-  } catch (error) {
-    console.error('Error while creating Form: ', error);
-    return false;
-  }
-}
-
-export async function readForm(formId) {
-  const Form = Parse.Object.extend('Form');
-  const query = new Parse.Query(Form);
-  query.equalTo('objectId', formId);
-  try {
-    const results = await query.find();
-    if (results[0].id !== undefined) {
-      return results[0];
-    }
-    return false;
-  } catch (error) {
-    console.error('Error while fetching Form', error);
-    return false;
+  if (!formRef.exists()) {
+    console.log('Form does not exists.');
+    return;
+  } else {
+    return formRef;
   }
 }
