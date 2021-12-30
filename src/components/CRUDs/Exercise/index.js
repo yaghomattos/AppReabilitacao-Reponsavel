b6d2a9a98e07a884e0242aab53d9c79ebc33a9a3
Yagho Mattos <mattosyagho@gmail.com>
2021-12-29 22:09:22 -0300

    feature: exercise firebase crud

--- src/components/CRUDs/Exercise/index.js
+++ src/components/CRUDs/Exercise/index.js
@@ -1,83 +1,40 @@
-import Parse from 'parse/react-native.js';
 import { Alert } from 'react-native';
 
 export async function createExercise(props) {
-  var { base64, fileName } = props.video;
-  fileName = props.name + '.gif';
-  const parseFile = new Parse.File(fileName, { base64 });
+  const participantRef = database.ref('participant');
 
-  var { base64, photoName } = props.photo;
-  photoName = props.name + '.png';
-  const parseFilePhoto = new Parse.File(photoName, { base64 });
+  participantRef
+    .push({
+      name: props.name,
+      description: props.description,
+      video: props.video,
+      preview: props.preview,
+    })
+    .then(() => {
+      console.log('Video criado');
+      Alert.alert('Video cadastrado');
+    });
+}
 
-  try {
-    const responseFile = await parseFile.save();
-    const photoFile = await parseFilePhoto.save();
+export async function readExercise(props) {
+  var exercise = '';
+  const exerciseRef = database
+    .ref('exercise')
+    .orderByChild('name')
+    .equalTo(props)
+    .on('child_added', function (snapshot) {
+      exercise = snapshot;
+    });
 
-    const File = Parse.Object.extend('Exercise');
-    const object = new File();
-    object.set('video', responseFile);
-    object.set('photo', photoFile);
-    object.set('name', props.name);
-    object.set('description', props.description);
-
-    await object.save();
-    Alert.alert('Exercício Salvo');
-  } catch (error) {
-    Alert.alert('Erro ao salvar exercício!');
+  if (!exercise) {
+    console.log('Exercise does not exists.');
+    return;
+  } else {
+    return exerciseRef;
   }
 }
 
-export async function readExercise(name) {
-  var result = '';
-  const Exercise = Parse.Object.extend('Exercise');
-  const query = new Parse.Query(Exercise);
-  query.equalTo('name', name);
-  try {
-    const results = await query.find();
-    for (const object of results) {
-      const video = object.get('video');
-      //console.log(video);
-    }
-    result = results;
-  } catch (error) {
-    console.error('Error while fetching Exercise', error);
-  }
-  return result[0];
-}
-
-export async function updateExercise() {
-  const query = new Parse.Query(Exercise);
-  try {
-    const object = await query.get('xKue915KBG');
-    object.set(
-      'video',
-      new Parse.File('resume.txt', { base64: btoa('My file content') })
-    );
-    try {
-      const response = await object.save();
-      console.log(response.get('video'));
-      console.log('Exercise updated', response);
-    } catch (error) {
-      console.error('Error while updating Exercise', error);
-    }
-  } catch (error) {
-    console.error('Error while retrieving object Exercise', error);
-  }
-}
-
-export async function deleteExercise() {
-  const query = new Parse.Query('Exercise');
-  try {
-    // here you put the objectId that you want to delete
-    const object = await query.get('xKue915KBG');
-    try {
-      const response = await object.destroy();
-      console.log('Deleted ParseObject', response);
-    } catch (error) {
-      console.error('Error while deleting ParseObject', error);
-    }
-  } catch (error) {
-    console.error('Error while retrieving ParseObject', error);
-  }
+export async function deleteExercise(props) {
+  const exerciseRef = database.ref('exercise/' + props.id);
+  exerciseRef.remove();
 }
