6a3a0fea8cbf7cd3865b352b915ef924edfa4b98
Yagho Mattos <mattosyagho@gmail.com>
2022-01-26 00:25:06 -0300

    feature: pre/post form crud

--- src/components/CRUDs/Form/index.js
+++ src/components/CRUDs/Form/index.js
@@ -1,7 +1,18 @@
 import { database } from '../../../services/firebase';
 
-export async function readForm(props) {
-  const formRef = await database.ref(`form/${props}`).get();
+export async function readPreForm(props) {
+  const formRef = await database.ref(`preForm/${props}`).get();
+
+  if (!formRef.exists()) {
+    console.log('Form does not exists.');
+    return false;
+  } else {
+    return formRef;
+  }
+}
+
+export async function readPostForm(props) {
+  const formRef = await database.ref(`postForm/${props}`).get();
 
   if (!formRef.exists()) {
     console.log('Form does not exists.');
