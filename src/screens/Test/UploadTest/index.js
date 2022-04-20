85fd2fd9f36508df9a5de149b2f37bb01a9ad25b
Yagho Mattos <mattosyagho@gmail.com>
2022-04-19 23:40:08 -0300

    fix: timer in upload test

--- src/screens/Test/UploadTest/index.js
+++ src/screens/Test/UploadTest/index.js
@@ -96,7 +96,7 @@ export function UploadTest() {
       preview: previewURL,
       name: name,
       description: description,
-      timer: timer,
+      timer: minutes * 60 + seconds,
       reps: reps,
     };
 
